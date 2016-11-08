cb7de6f0fb800fb63f1b7157ef3c343b13152e8b
kanghonggu <kanghonggu@zuminternet.com>
2016-11-08 18:00:23 +0900

    -

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
-//var mecab = require('mecab-ffi');
 var nodeSummary = require('node-summary');
+var mecab = require('mecab-ya');
+
 
 var news = require('./routes/news');
 
@@ -9,10 +10,24 @@ var port = 3000;
 
 app.use('/news/', news);
 
-// mecab.parse('한글 형태소 분석기를 테스트', function(err, result) {
-//   console.log (result);
-// });
+
+
+
 
 app.listen(port, function () {
   console.log("Server is runnig on " + port);
+
+  var text = '아버지가방에들어가신다';
+
+  mecab.pos(text, function (err, result) {
+      console.log(result);
+      /*
+          [ [ '아버지', 'NNG' ],
+            [ '가', 'JKS' ],
+            [ '방', 'NNG' ],
+            [ '에', 'JKB' ],
+            [ '들어가', 'VV' ],
+            [ '신다', 'EP+EC' ] ]
+      */
+  });
 });
